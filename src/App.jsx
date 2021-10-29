4ef35687afdd0a26d6c7315fc112bf8b775d713c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 11:08:00 -0300

    Feat: Cria as rotas no componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Offer from './pages/Offer';
 
 function App() {
-  return <h1>FIoravante</h1>;
+  return (
+    <Switch>
+      <Route component={Home} exact path="/" />
+      <Route component={Offer} exact path="/offers" />
+    </Switch>
+  );
 }
 
 export default App;
