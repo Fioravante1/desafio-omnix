10c49e4e0dfa67089baffe1a79100af8ea33951f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 11:19:46 -0300

    Feat: Cria o componente home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormCep from '../components/form/FormCep';
+import Header from '../components/header/Header';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <FormCep />
+    </div>
+  );
+}
+
+export default Home;
