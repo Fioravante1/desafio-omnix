c7ea78431cb9af63b87c36624f1b62e1d0e6de3c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-29 11:09:27 -0300

    Feat: Adiciona o provider e browserRouter no doc index

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Provider from './context/Provider';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
